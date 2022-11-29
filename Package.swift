38ccca9d3cc4019307ef60afaba6e80e81944571
Michał Kaczmarowski <michal@movi.ai>
2022-11-29 16:21:13 +0100

    Update package swift-tools-version to 5.2, and update ios version to 13

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.2
 //
 //  Package.swift
 //  NextLevel (http://nextlevel.engineering/)
@@ -28,7 +28,7 @@ import PackageDescription
 let package = Package(
     name: "NextLevel",
     platforms: [
-      .iOS(.v14)
+      .iOS(.v13)
     ],
     products: [
       .library(name: "NextLevel", targets: ["NextLevel"])
